d3afab7974915c88b2f4b9380be10683eddff099
ConnerKT <connerthompson121@gmail.com>
2023-02-20 13:36:24 -0600

    Made Assignment

---
+++ script.js
@@ -0,0 +1,6 @@
+let answ1=prompt("What is your name");
+let answ2=prompt("What is your favorite color?");
+let answ3=prompt("Whats your favorite snack?");
+let answ4=prompt("What is one of your hobbies?");
+
+alert(answ1+"'s" +" "+"favorite color is"+" "+answ2+"."+" "+answ3+" "+"is"+" "+ answ1+"'s" +" "+"favorite snack"+"."+" "+ answ1+"'s"+" "+"loves" +" "+ answ4+".");
